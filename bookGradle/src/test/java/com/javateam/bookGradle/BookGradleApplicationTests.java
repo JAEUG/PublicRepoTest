29125b273c4ba9a1a257621b140834394011f33c
JAEUG <161946591+JAEUG@users.noreply.github.com>
2024-03-04 21:05:55 +0900

    Update BookGradleApplicationTests.java

--- bookGradle/src/test/java/com/javateam/bookGradle/BookGradleApplicationTests.java
+++ bookGradle/src/test/java/com/javateam/bookGradle/BookGradleApplicationTests.java
@@ -3,6 +3,8 @@ package com.javateam.bookGradle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// 여기에 주석이 생긴걸 알아차릴수 있을까요?
+
 @SpringBootTest
 class BookGradleApplicationTests {
 
